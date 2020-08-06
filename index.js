0ed046f2c4e6737a20f95c9f0b121298106fa34a
Taylor Darneille <tmdarneille@gmail.com>
2020-08-06 12:02:58 -0700

    finished GET and POST routes, minus the get edit form one

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fs = require('fs') // will use to read json files
 // tell express we're using ejs
 app.set('view engine', 'ejs')
 app.use(ejsLayouts) // tell express to let us use a layout template
+app.use(express.urlencoded({extended: false})) // body-parser middleware
 
 // home route
 app.get('/', (req, res)=>{
@@ -19,6 +20,17 @@ app.get('/dinosaurs', (req, res)=>{
     let dinosaurs = fs.readFileSync('./dinosaurs.json')
     // convert the json to javascript
     let dinoData = JSON.parse(dinosaurs)
+
+    let nameFilter = req.query.nameFilter
+
+    // keep in dinoData any dinos whose name matches the 
+    // nameFilter the user searched for
+    if(nameFilter){
+        dinoData = dinoData.filter((dino)=>{
+            return dino.name.toLowerCase()===nameFilter.toLowerCase()
+        })
+    }
+    
     // render our dino index page and pass it the
     // dinoData as "myDinos"
     res.render('dinosaurs/index', {myDinos: dinoData})
@@ -36,6 +48,19 @@ app.get('/dinosaurs/:id', (req, res)=>{
     // grab the id parameter from the url and convert to int (was string originally)
     let dinoIndex = parseInt(req.params.id)
     res.render('dinosaurs/show', {myDino: dinoData[dinoIndex]})
+})
+
+// post a new dino!
+app.post('/dinosaurs', (req, res)=>{
+    // get json dinos and convert to a js array of objects
+    let dinosaurs = fs.readFileSync('./dinosaurs.json')
+    let dinoData = JSON.parse(dinosaurs)
+    // push new dino to the array
+    dinoData.push(req.body)
+    // convert dinoData back to JSON and write to dinosaurs.json file
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+    // redirect to the index get route
+    res.redirect('/dinosaurs')
 })
 
 
